3bd468d4a9f3b6da1993a9a1740af5118c93f46b
purva baviskar <purvabaviskar13@gmail.com>
2025-09-08 01:27:52 +0530

    Updated UI for full screen on mobile12

--- chart.js
+++ chart.js
@@ -1,1 +1,6 @@
-<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js"></script>
+import Chart from 'chart.js/auto';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getFirestore, doc, getDoc, updateDoc, arrayUnion, Timestamp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+// import Chart from 'https://cdn.jsdelivr.net/npm/chart.js/dist/chart.esm.js';
+    const score = getScoreFromCoordinates(x, y);
